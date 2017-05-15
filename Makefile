f76cc2319062907ac605b41236464e43d4fc6d7a
truncator <truncator0@gmail.com>
2017-05-15 16:40:20 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,29 @@
+BINARY_NAME=klang
+BINARY_DIR=bin
+BUILD_DIR=build
+
+COMPILER=clang
+COMPILER_FLAGS=-m64 -std=c++11 -O0 -g -DPLATFORM_LINUX \
+			   -Isrc \
+			   -Werror -Wall -Wpedantic -Wshadow \
+			   -Wno-missing-braces -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable
+LINKER_FLAGS=-m64 -fuse-ld=gold -Lbin
+
+SOURCES:=$(wildcard src/*.cpp)
+HEADERS:=$(wildcard src/*.h)
+OBJECTS:=$(patsubst src/%.cpp, $(BUILD_DIR)/%.o, $(SOURCES))
+
+.PHONY: default
+default: main
+
+main: $(OBJECTS)
+	@mkdir -p bin
+	@$(COMPILER) $(COMPILER_FLAGS) $(LINKER_FLAGS) $(OBJECTS) -o $(BINARY_DIR)/$(BINARY_NAME)
+
+$(BUILD_DIR)/%.o: src/%.cpp $(HEADERS)
+	@mkdir -p build
+	@$(COMPILER) $(COMPILER_FLAGS) -c $< -o $@
+
+.PHONY: clean
+clean:
+	@rm -rf $(BINARY_DIR) $(BUILD_DIR)
---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#include <stdint.h>
+typedef int32_t i32;
+
+static char *read_file(char *path)
+{
+    FILE *file = fopen(path, "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: missing input file \"%s\"\n", path);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *result = (char *)malloc(length + 1);
+    fread(result, length, 1, file);
+    result[length] = '\0';
+
+    fclose(file);
+
+    return result;
+}
+
+static void print_help()
+{
+    fprintf(stderr, "Usage: klang <source>\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        print_help();
+    }
+
+    for (int i = 1; i < argc; ++i)
+    {
+        char *file = read_file(argv[i]);
+        printf("%s\n", file);
+    }
+
+    return 0;
+}
