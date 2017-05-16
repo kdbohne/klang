b076198e2c6461f1b82605e6f420be88e0634021
truncator <truncator0@gmail.com>
2017-05-15 21:52:42 -0500

    [SQUASH] Initial AST

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,54 @@
 #include "common.h"
 #include "array.h"
 
+struct AstRoot;
+struct AstFunc;
+struct AstLit;
+
+enum AstNodeType : u32
+{
+    AST_ROOT,
+    AST_FUNC,
+
+    // Expr
+    AST_LIT,
+};
+
+struct AstRoot
+{
+    Array<AstFunc> funcs;
+};
+
+struct AstFunc
+{
+    i32 placeholder;
+};
+
+enum AstLitType
+{
+    AST_LIT_INT,
+};
+
+struct AstLit
+{
+    AstLitType type;
+    union
+    {
+        u64 value_int;
+    };
+};
+
+struct AstNode
+{
+    AstNodeType type;
+    union
+    {
+        AstRoot root;
+        AstFunc func;
+        AstLit lit;
+    };
+};
+
 static char *read_file(char *path)
 {
     FILE *file = fopen(path, "r");
