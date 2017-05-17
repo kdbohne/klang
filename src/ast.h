78d19271737892aa7f7b40c02fecc58ffbb45eff
truncator <truncator0@gmail.com>
2017-05-16 20:13:11 -0500

    Move AST types to own file

---
+++ src/ast.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "common.h"
+
+struct AstRoot;
+struct AstFunc;
+struct AstLit;
+
+enum AstNodeType : u32
+{
+    AST_ROOT,
+    AST_FUNC,
+
+    // Expr
+    AST_LIT,
+};
+
+struct AstRoot
+{
+    Array<AstFunc> funcs;
+};
+
+struct AstFunc
+{
+    i32 placeholder;
+};
+
+enum AstLitType
+{
+    AST_LIT_INT,
+};
+
+struct AstLit
+{
+    AstLitType type;
+    union
+    {
+        u64 value_int;
+    };
+};
+
+struct AstNode
+{
+    AstNodeType type;
+    union
+    {
+        AstRoot root;
+        AstFunc func;
+        AstLit lit;
+    };
+};
