1c711abe628e4f04d290771242549b9ecab8ed54
truncator <truncator0@gmail.com>
2017-05-22 22:17:53 -0500

    Add string length/copy/duplicate utility functions

--- src/core/string.cpp
+++ src/core/string.cpp
@@ -22,3 +22,30 @@ bool strings_match(const char *a, const char *b)
 {
     return strcmp(a, b) == 0;
 }
+
+int string_length(const char *str)
+{
+    int len = 0;
+    while (*str++)
+        ++len;
+
+    return len;
+}
+
+void string_copy(const char *src, char *dest)
+{
+    while ((*dest++ = *src++) != '\0');
+}
+
+extern "C" { void *malloc(u64 size); }
+char *string_duplicate(const char *str)
+{
+    int len = string_length(str);
+
+    // TODO: smarter allocation
+    char *dup = (char *)malloc(len + 1);
+    string_copy(str, dup);
+    dup[len] = '\0';
+
+    return dup;
+}
--- src/core/string.h
+++ src/core/string.h
@@ -5,3 +5,7 @@
 u64 hash_djb2(const char *str);
 
 bool strings_match(const char *a, const char *b);
+
+int string_length(const char *str);
+void string_copy(const char *src, char *dest);
+char *string_duplicate(const char *str);
