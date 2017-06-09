1b94f0693ce48fb5f7d6fe4ce8b174a79eb4f80c
truncator <truncator0@gmail.com>
2017-06-09 14:12:25 -0500

    Clean up syscall

--- lang/core/syscall.s
+++ lang/core/syscall.s
@@ -11,26 +11,3 @@ syscall5:
 
     syscall
     ret
-
-/*
-.global _start
-_start:
-    xor %rbp, %rbp
-    and -16, %rsp
-    call main
-    ret
-*/
-/*
-_start:
-    xor %rbp, %rbp
-    pop %rdi
-    movq %rsp, %rsi
-    and %rsp, -16
-    call main
-
-    movq %rax, %rdi
-    movq $60, %rax
-    syscall
-
-    ret
-*/
