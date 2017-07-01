65a83af1a1b8c98d9219ff287ad9900dd68ceef3
truncator <truncator0@gmail.com>
2017-07-01 17:35:07 -0500

    Initial bitwise operator functions

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 # Compile core utils
 clang -m64 -c lang/core/start.s -o build/start.o
 clang -m64 -c lang/core/syscall.s -o build/syscall.o
+clang -m64 -c lang/core/bit.c -o build/bit.o
 
 # FIXME: pass backend choice to klang
 ir=$(./bin/klang test/first.k)
@@ -24,7 +25,7 @@ else
     exit
 fi
 
-clang -m64 -nostartfiles build/first.o build/start.o build/syscall.o -o ./bin/first
+clang -m64 -nostartfiles build/first.o build/start.o build/syscall.o build/bit.o -o ./bin/first
 #clang -m64 -nostartfiles -nostdlib build/first.o build/start.o build/syscall.o -o ./bin/first
 
 #rm build/first.o build/syscall.o
---
+++ lang/core/bit.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+uint64_t bit_and(uint64_t a, uint64_t b) {
+    return a & b;
+}
+
+uint64_t bit_or(uint64_t a, uint64_t b) {
+    return a | b;
+}
+
+uint64_t bit_shift_left(uint64_t x, uint64_t amount) {
+  return x << amount;
+}
+
+uint64_t bit_shift_right(uint64_t x, uint64_t amount) {
+  return x >> amount;
+}
