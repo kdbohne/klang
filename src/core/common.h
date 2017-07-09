c2b6a2174fd3aeb7cafce2eed8eb247d74bbd8f2
truncator <truncator0@gmail.com>
2017-07-08 19:22:01 -0500

    Add more primitive typedefs

--- src/core/common.h
+++ src/core/common.h
@@ -5,10 +5,16 @@ void assert_(bool cond, const char *cond_str, const char *file, unsigned line);
 #define assert(cond) assert_(cond, #cond, __FILE__, __LINE__)
 
 #include <stdint.h>
+typedef int8_t  i8;
+typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef float  f32;
+typedef double f64;
 
 #undef NULL
 #define NULL 0
